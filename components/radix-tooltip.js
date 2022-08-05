7d1b6aff88cf38bb098a05edac80436dbc49d47c
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-08-05 16:41:36 -0300

    Add radix tooltip

---
+++ components/radix-tooltip.js
@@ -0,0 +1,15 @@
+import * as Tooltip from '@radix-ui/react-tooltip'
+
+export default function RadixTooltip({ children, content }) {
+  return (
+    <Tooltip.Provider delayDuration={500} skipDelayDuration={0}>
+      <Tooltip.Root>
+        <Tooltip.Trigger>{children}</Tooltip.Trigger>
+        <Tooltip.Content className="px-2 py-1 text-slate-900 bg-slate-100 rounded">
+          {content}
+          <Tooltip.TooltipArrow className="fill-slate-100" />
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  )
+}
