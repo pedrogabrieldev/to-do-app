019bea3a6b40ae32b5749719ea648fe7f4d45564
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-08-08 22:17:03 -0300

    Implement authentication with google account

--- components/login.js
+++ components/login.js
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from 'firebase/auth'
 import { collection, addDoc } from 'firebase/firestore'
 import { auth, db } from '../lib/firebase'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 export default function Login() {
@@ -13,6 +16,29 @@ export default function Login() {
   const [password, setPassword] = useState('')
   const [isLogin, setIsLogin] = useState(true)
   const [isLoading, setIsLoading] = useState(false)
+
+  async function handleLoginWithGoogle() {
+    try {
+      const provider = new GoogleAuthProvider()
+      provider.setCustomParameters({
+        prompt: 'select_account',
+      })
+      const result = await signInWithPopup(auth, provider)
+      const credential = GoogleAuthProvider.credentialFromResult(result)
+      const token = credential.accessToken
+    } catch (error) {
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.customData.email
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error)
+      console.log(errorCode)
+      console.log(errorMessage)
+      console.log(email)
+      console.log(credential)
+    }
+  }
 
   async function handleSubmit(event) {
     event.preventDefault()
@@ -57,6 +83,15 @@ export default function Login() {
         className="flex-1 flex flex-col justify-center items-center gap-2 w-full max-w-xs"
         onSubmit={handleSubmit}
       >
+        <button
+          type="button"
+          className="w-full px-3 py-2 text-[#0D0D0D] bg-[#F2F2F2] rounded-md"
+          onClick={handleLoginWithGoogle}
+        >
+          <FontAwesomeIcon icon={faGoogle} />
+          <span className="ml-3">Login with Google</span>
+        </button>
+        <span className="my-3">or</span>
         <input
           type="email"
           className="text-[#F2F2F2] px-3 py-2 outline-none border border-[#333333] rounded-md w-full bg-[#262626]"
@@ -75,7 +110,7 @@ export default function Login() {
         />
         <button
           type="submit"
-          className="w-full my-2 px-3 py-2 text-[#F2F2F2] text-xl font-medium border border-[#333333] rounded-md cursor-pointer duration-500 hover:bg-indigo-500"
+          className="w-full my-2 px-3 py-2 bg-indigo-500 text-[#F2F2F2] text-xl rounded-md cursor-pointer duration-500 hover:bg-indigo-600"
         >
           {isLoading ? (
             <FontAwesomeIcon icon={faSpinner} spin />
