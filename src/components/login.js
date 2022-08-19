5064b59b6a56d6eec763d567074def2a5517470c
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-08-19 10:13:35 -0300

    Add login with github

--- src/components/login.js
+++ src/components/login.js
@@ -1,41 +1,28 @@
-import { useState } from 'react'
-import { signInWithRedirect, GoogleAuthProvider } from 'firebase/auth'
-import { auth, db } from '../lib/firebase'
+import { useUser } from '../context/userContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons'
 
 export default function Login() {
-  async function handleLoginWithGoogle() {
-    try {
-      const provider = new GoogleAuthProvider()
-      provider.setCustomParameters({
-        prompt: 'select_account',
-      })
-      await signInWithRedirect(auth, provider)
-    } catch (error) {
-      const errorCode = error.code
-      const errorMessage = error.message
-      // The email of the user's account used.
-      const email = error.customData.email
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error)
-      console.log(errorCode)
-      console.log(errorMessage)
-      console.log(email)
-      console.log(credential)
-    }
-  }
+  const { loginWithGithub, loginWithGoogle } = useUser()
 
   return (
     <>
-      <div className="flex-1 flex flex-col justify-center items-center gap-2 w-full max-w-xs">
+      <div className="flex-1 flex flex-col justify-center items-center gap-3 w-full max-w-xs">
         <button
           type="button"
-          className="w-full px-3 py-2 text-[#0D0D0D] bg-[#F2F2F2] border border-[#333333] shadow-lg rounded-md dark:border-none dark:shadow-none"
-          onClick={handleLoginWithGoogle}
+          className="flex justify-center items-center w-full px-3 py-2 text-[#0D0D0D] bg-[#F2F2F2] border border-[#333333] shadow-lg rounded-md dark:border-none dark:shadow-none"
+          onClick={loginWithGoogle}
         >
-          <FontAwesomeIcon icon={faGoogle} />
+          <FontAwesomeIcon icon={faGoogle} size="xl" />
           <span className="ml-3">Login with Google</span>
+        </button>
+        <button
+          type="button"
+          className="flex justify-center items-center w-full px-3 py-2 text-[#0D0D0D] bg-[#F2F2F2] border border-[#333333] shadow-lg rounded-md dark:border-none dark:shadow-none"
+          onClick={loginWithGithub}
+        >
+          <FontAwesomeIcon icon={faGithub} size="xl" />
+          <span className="ml-3">Login with Github</span>
         </button>
       </div>
     </>
--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,5 +1,11 @@
 import { useState, useEffect, createContext, useContext } from 'react'
-import { onAuthStateChanged, signOut } from 'firebase/auth'
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithRedirect,
+  signOut,
+} from 'firebase/auth'
 import { auth } from '../lib/firebase'
 
 export const UserContext = createContext()
@@ -7,6 +13,44 @@ export const UserContext = createContext()
 export default function UserProvider({ children }) {
   const [user, setUser] = useState(null)
   const [isLoadingUser, setIsLoadingUser] = useState(true) // Helpful, to update the UI accordingly.
+
+  async function loginWithGithub() {
+    try {
+      const provider = new GithubAuthProvider()
+      provider.setCustomParameters({
+        prompt: 'select_account',
+      })
+      signInWithRedirect(auth, provider)
+    } catch (error) {
+      const errorCode = error.code
+      const errorMessage = error.message
+      const email = error.customData.email
+      const credential = GithubAuthProvider.credentialFromError(error)
+      console.log(errorCode)
+      console.log(errorMessage)
+      console.log(email)
+      console.log(credential)
+    }
+  }
+
+  async function loginWithGoogle() {
+    try {
+      const provider = new GoogleAuthProvider()
+      provider.setCustomParameters({
+        prompt: 'select_account',
+      })
+      signInWithRedirect(auth, provider)
+    } catch (error) {
+      const errorCode = error.code
+      const errorMessage = error.message
+      const email = error.customData.email
+      const credential = GoogleAuthProvider.credentialFromError(error)
+      console.log(errorCode)
+      console.log(errorMessage)
+      console.log(email)
+      console.log(credential)
+    }
+  }
 
   async function signOutUser() {
     try {
@@ -39,7 +83,16 @@ export default function UserProvider({ children }) {
   }, [])
 
   return (
-    <UserContext.Provider value={{ user, setUser, isLoadingUser, signOutUser }}>
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        isLoadingUser,
+        loginWithGithub,
+        loginWithGoogle,
+        signOutUser,
+      }}
+    >
       {children}
     </UserContext.Provider>
   )
